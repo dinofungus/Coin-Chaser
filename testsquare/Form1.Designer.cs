a45c3cea02f3ec8a8989942dd36d68e98e595b9a
dinofungus <dinofungus@hotmail.com>
2017-05-28 21:31:52 +1000

    Initial commit

---
+++ testsquare/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace CoinChaser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(20, 20);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(86, 31);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Press arrow key to begin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 301);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ testsquare/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoinChaser
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        private int playerX = 100;
+        private int playerY = 100;
+
+        private int coinX;
+        private int coinY;
+
+        private int score = 0;
+
+        Panel game = new Panel();
+
+
+        public Form1()
+        {
+            
+                      
+            game.Parent = this;
+            game.Top = 60;
+            game.Left = 20;
+            game.Height = 220;
+            game.Width = 240;
+            
+            InitializeComponent();
+            lblScore.Text = "Score: " + score;
+            NewCoin();
+            
+            label1.BringToFront();
+            game.Show();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = game.CreateGraphics();
+            graphics.Clear(Color.Blue);
+
+            SolidBrush brush = new SolidBrush(Color.Red);
+            graphics.FillRectangle(brush, playerX, playerY, 20, 20);
+
+            brush = new SolidBrush(Color.Gold);
+            graphics.FillRectangle(brush, coinX, coinY, 20, 20);
+
+            brush.Dispose();
+            graphics.Dispose();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            game.BringToFront();
+            if (e.KeyCode == Keys.Up)
+            {
+                if (playerY > 0) playerY -= 20;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (playerY < game.Height - 20) playerY += 20;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                if (playerX > 0) playerX -= 20;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (playerX < game.Width - 20) playerX += 20;
+            }
+
+            if(coinX == playerX && coinY == playerY)
+            {
+                NewCoin();
+                score++;
+                lblScore.Text = "Score: " + score;
+            }
+
+            Form1_Paint(this, null);
+
+        }
+        private void NewCoin()
+        {
+            do
+            {
+                coinX = random.Next(1, game.Width / 20) * 20;
+                coinY = random.Next(1, game.Height / 20) * 20;
+            } while (coinX == playerX && coinY == playerY);
+            Form1_Paint(this, null);
+        }
+    }
+}
