ec6cfa607a406865851052b6d74055c7d09176b6
dinofungus <dinofungus@hotmail.com>
2017-05-28 23:44:34 +1000

    Changed namespace and assembly name to CoinChaser

--- CoinChaser/Form1.Designer.cs
+++ CoinChaser/Form1.Designer.cs
@@ -59,7 +59,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblScore);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Coin Chaser";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
--- CoinChaser/Properties/Resources.Designer.cs
+++ CoinChaser/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace CoinChaser.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("testsquare.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CoinChaser.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
